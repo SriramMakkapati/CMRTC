eca5f0d0c2cc1da39f46a691ef94cd4771899cac
Sriram Makkapati <79370901+SriramMakkapati@users.noreply.github.com>
2021-02-23 19:27:42 +0530

    Add files via upload

---
+++ maxproductsubarray.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int maximumProduct(int A[],int n) {
+    int i;
+    int minimum, maximum, x;
+    x = maximum = minimum = A[0];
+    for(i = 1; i < n; i++)
+ {
+        int t_maximum = maximum;
+        if (maximum * A[i] > A[i])
+            maximum *= A[i];
+        else
+            maximum = A[i];
+        if (minimum * A[i] > maximum)
+            maximum = minimum * A[i];
+        if (minimum * A[i] < A[i])
+            minimum *= A[i];
+        else
+            minimum = A[i];
+        if (t_maximum * A[i] < minimum)
+            minimum = t_maximum * A[i];
+        if (maximum > x)
+            x = maximum;
+    }
+    return x;
+}
+int main() {
+    int A[] = {2,5,-9,-6,2};
+    printf("%d\n", maximumProduct(A, 5));
+    return 0;
+}
