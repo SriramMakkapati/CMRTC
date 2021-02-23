d5abb577c61a556f22bb5b3e46d8f695f6c01606
Sriram Makkapati <79370901+SriramMakkapati@users.noreply.github.com>
2021-02-23 19:47:33 +0530

    Update maxproductsubarray.c

--- maxproductsubarray.c
+++ maxproductsubarray.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int maximumProduct(int A[],int n) {
     int i;
-    int minimum, maximum, x;
-    x = maximum = minimum = A[0];
+    int minimum,maximum,x;
+    x= maximum = minimum = A[0];
     for(i = 1; i < n; i++)
  {
         int t_maximum = maximum;
-        if (maximum * A[i] > A[i])
+        if(maximum * A[i] > A[i])
             maximum *= A[i];
         else
             maximum = A[i];
-        if (minimum * A[i] > maximum)
+        if(minimum * A[i] > maximum)
             maximum = minimum * A[i];
-        if (minimum * A[i] < A[i])
+        if(minimum * A[i] < A[i])
             minimum *= A[i];
         else
             minimum = A[i];
-        if (t_maximum * A[i] < minimum)
+        if(t_maximum * A[i] < minimum)
             minimum = t_maximum * A[i];
-        if (maximum > x)
+        if(maximum > x)
             x = maximum;
     }
     return x;
